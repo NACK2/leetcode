3b47623c9144df86be71dc17821bd03fad65b4fc
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-06-22 20:31:23 -0700

    Time: 34 ms (22.55%), Space: 20 MB (15.20%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        
+        for (int i=0; i<nums.size(); ++i) {
+            ++counts[nums[i]];
+        }
+        
+        for (auto count : counts) {
+            if (count.second == 1) 
+                return count.first;
+        }
+        
+        return 0;
+    }
+};
