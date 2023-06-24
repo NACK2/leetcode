a0f8e0ec33c628844d779b72dce393f1cfb2509f
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-06-23 22:03:47 -0700

    Time: 6 ms (20.47%), Space: 6.7 MB (14.65%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        // sort(sentence.begin(); sentence.end())
+        unordered_map<char, int> counts;
+        
+        for (int i=0; i<sentence.length(); ++i) {
+            ++counts[sentence[i]];
+        }
+        
+        char tmp = 'a';
+        while (tmp <= 'z') {
+            if (!counts[tmp]) 
+                return false;
+            ++tmp;
+        }
+        
+        return true;
+    }
+};
