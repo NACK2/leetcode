b0f4bd2aa8528219ced06f7d34ad33d7c398a473
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-06-23 18:41:21 -0700

    Time: 11 ms (39.47%), Space: 6.8 MB (91.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,28 +3,22 @@ public:
     bool isIsomorphic(string s, string t) {
         if (s.length() != t.length())
             return false;
-        
-        // int sPtr = 0;
-        // int tPtr = 0;
-        unordered_map<char, char> sToT;
-        
+
+        unordered_map<char, char> sToT;  
         for (int i=0; i<s.length(); ++i) {
-            if (sToT.find(s[i]) == sToT.end()) {
+            if (sToT.find(s[i]) == sToT.end()) // if key doesnt exist
                 sToT[s[i]] = t[i];
-            } 
-            else if (sToT[s[i]] != t[i] ) 
+            else if (sToT[s[i]] != t[i] ) // key exists but is mapped to smth else
                 return false;
         }
         
         unordered_map<char, char> tToS;
         for (int i=0; i<t.length(); ++i) {
-            if (tToS.find(t[i]) == tToS.end()) {
+            if (tToS.find(t[i]) == tToS.end()) 
                 tToS[t[i]] = s[i];
-            } 
             else if (tToS[t[i]] != s[i] ) 
                 return false;
         }
-        
         
         
         return true;
