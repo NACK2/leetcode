b0f2f091971548aca57ff81732b1e7cfca8807d2
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-06-23 21:55:00 -0700

    Time: 75 ms (12.71%), Space: 30.2 MB (31.29%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
         unordered_map<int, int> counts;
-        int ret = 0;
         
         for (int i=0; i<nums.size(); ++i) {
             ++counts[nums[i]];
-            if (counts[nums[i]] == nums.size()/2) {
-                ret = nums[i];
-                break;
+        }
+        
+        for (auto count : counts) {
+             if (count.second == nums.size()/2) {
+                return count.first;
             }
         }
         
-        return ret;
+        return 0;
     }
 };
