6e1c004320db3d31e7d38a8f45c834e5ed1ddd6d
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-06-24 18:11:59 -0700

    Time: 118 ms (10.88%), Space: 19.1 MB (98.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -7,7 +7,7 @@ public:
             if (!nums[i]) {
                 ++count;
                 nums.erase(nums.begin()+i);
-                --i; // move index back bc erased element
+                --i; // move i back bc erased element
             }
         }
         
