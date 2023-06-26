92bcb63fb066c0cd80471854cff0a96066dc6183
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-06-25 17:36:58 -0700

    Time: 48 ms (42.37%), Space: 90 MB (24.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -11,7 +11,7 @@ public:
         sort(tmp.begin(), tmp.end());
         int n = tmp.size();
         
-        // if odd size return middle element, 
+        // if odd size return middle element 
         // if even return median of two middle elements
         return n%2==1 ? tmp[n/2] : (tmp[n/2] + tmp[n/2-1])/2.0; 
     }
