2cea52b91411e7b3de62af3190e37204465d9045
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-06-28 12:29:44 -0700

    Time: 33 ms (11.58%), Space: 15.2 MB (22.94%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (!head) 
+            return head;
+        
+        ListNode *curr = head;
+        ListNode *toDel = NULL;
+        
+        while (curr->next) {
+            if (curr->next->val == val) {
+                toDel = curr->next;
+                curr->next = toDel->next;
+                delete toDel;
+            }
+            else {
+                curr = curr->next;
+            }
+        }
+        
+        // if first val has to be deleted
+        curr = head;
+        if (curr->val == val) {
+            head = head->next;
+            delete curr;
+        }
+        
+        return head;
+    }
+};
