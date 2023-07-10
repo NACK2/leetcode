ebc346dbfe544380c1ee2d529827d7dfd3130be8
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-07-10 13:07:07 -0700

    Time: 4 ms (40.56%), Space: 6.3 MB (77.56%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        
+        for (int i=0; i<s.length(); ++i) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') 
+                stack.push(s[i]);
+            else if (stack.empty())
+                return false;
+            else if (s[i] == ')' && stack.top() != '(')
+                return false;
+            else if (s[i] == ']' && stack.top() != '[')
+                return false;
+            else if (s[i] == '}' && stack.top() != '{')
+                return false;
+            else 
+                stack.pop();
+        }
+        
+        if (stack.empty()) 
+            return true;
+
+        return false;
+    }
+};
