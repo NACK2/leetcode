a538f195c3666d62ca690353ed4e91407a2fe7e7
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-09-17 20:44:59 -0700

    Time: 30 ms (21.03%), Space: 23.8 MB (7.98%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,22 +12,14 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         TreeNode *curr = root;
-        int l = p->val;
-        int r = q->val;
-        if (p->val > q->val) {
-            l = q->val;
-            r = p->val;
-        }
         
         while (curr) {
-            if (curr->val > l && curr->val < r) 
-                return curr;
-            else if (curr->val == l || curr->val == r) 
-                return curr;
-            else if (curr->val > l && curr->val > r)
+            if (curr->val > p->val && curr->val > q->val)
                 curr = curr->left;
-            else 
+            else if (curr->val < p->val && curr->val < q->val)
                 curr = curr->right;
+            else // either ancestor is right between the two or ancestor == p or ancestor == q
+                return curr;
         }
         
         return NULL;
