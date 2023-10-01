6e8eae3d18b63efdfee88f2fc2b1e0717e620585
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-10-01 00:11:02 -0700

    Time: 34 ms (78.08%), Space: 16.3 MB (20.07%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,12 +3,12 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        left = 1
+        left = 1 # not 1 based indexing
         right = n
         mid = 0
         
         while (left <= right): 
-            mid = (right - left)//2 + left
+            mid = (right - left)//2 + left # dont forget int division
 
             if not isBadVersion(mid-1) and isBadVersion(mid): # found
                 return mid
