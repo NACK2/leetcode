127b4b8ead9bc56a1ef9f5079ecbc48ad0a2a389
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-10-02 15:07:49 -0700

    Time: 47 ms (21.33%), Space: 16.3 MB (34.35%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,23 +1,19 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = a[::-1] # reversing bc addition starts from back
-        b = b[::-1]
-        numA, numB, total, carry = 0, 0, 0, 0
+        a, b = a[::-1], b[::-1]
+        total, carry, numA, numB = 0, 0, 0, 0
         res = ""
         
         for i in range(max(len(a), len(b))):
-            numA = int(a[i]) if i < len(a) else 0 # using ascii values to turn char into int
-            numB = int(b[i]) if i < len(b) else 0
+            numA = ord(a[i]) - ord('0') if i < len(a) else 0 # using ascii to convert str into int
+            numB = ord(b[i]) - ord('0') if i < len(b) else 0
             
-            total = numA + numB + carry # first iteration carry will be 0 by default
-            res += str(total % 2)
-            carry = total // 2 # either 0 or 1
+            total = numA + numB + carry
+            res += str(total%2)
+            carry = total // 2
         
-        if carry == 1:# last digit had a carry
-            res += "1"
+        if carry == 1:
+            res += '1'
         
         return res[::-1]
             
-            
-
-                
