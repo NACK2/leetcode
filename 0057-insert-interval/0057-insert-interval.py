148dfe82a39a9094100919579228c4c97747b5f9
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-10-10 13:37:08 -0700

    Time: 82 ms (62.26%), Space: 19.5 MB (98.99%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,17 +1,16 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        intervalIndex = 0
         
         for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]: # end of newInterval and start of curr interval dont overlap
+            if newInterval[1] < intervals[i][0]: # newInterval end < curr interval start
                 res.append(newInterval)
                 return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]: # start of newInterval and end of curr interval dont overlap
+            elif newInterval[0] > intervals[i][1]: # newInterval start > curr interval end
                 res.append(intervals[i])
-            else: # some sort of overlap, take min of curr and newInterval start, and max of curr and newInterval end
+            else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-            
-        res.append(newInterval) # adding updated interval or possible case where newInterval > all intervals
-                
+        
+        res.append(newInterval)
+        
         return res
