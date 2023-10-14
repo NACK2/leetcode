8cb5262b9a1531b97a7b5f477505d6fafdf06223
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-10-14 15:34:26 -0700

    Time: 316 ms (36.40%), Space: 24.1 MB (37.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,25 @@
 class Solution:
-    def bfs(self, grid, r, c):
-        q = [];
-        self.visited.add((r, c))
-        q.append((r, c))
+    def dfs(self, grid, r, c):
+        if r == len(grid) or r == -1 or c == len(grid[0]) or c == -1 or grid[r][c] == "0" or ((r, c)) in self.visited:
+            return
         
-        while q:
-            pairIndex = q.pop(0)
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            
-            # exploring adjacent nodes, checking if its land and not visited
-            for i in directions:
-                dr, dc = i[0], i[1]
-                r = pairIndex[0] + dr
-                c = pairIndex[1] + dc
-                if r < len(grid) and c < len(grid[0]) and r > -1 and c > -1 and grid[r][c] == "1" and (r, c) not in self.visited:
-                    self.visited.add((r, c))
-                    q.append((r, c))
+        self.visited.add((r, c))
+        self.dfs(grid, r+1, c)
+        self.dfs(grid, r-1, c)
+        self.dfs(grid, r, c+1)
+        self.dfs(grid, r, c-1)
                 
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: 
-            return 0
-        
+        islands = 0
+        self.visited = set()
         rows = len(grid)
         cols = len(grid[0])
-        islands = 0
-        self.visited = set() # (r, c) is in visited if we have visited it before
-
+        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in self.visited:
-                    self.bfs(grid, r, c)
+                if grid[r][c] == "1" and ((r, c)) not in self.visited:
+                    self.dfs(grid, r, c)
                     islands += 1
-                    
+        
         return islands
                 
