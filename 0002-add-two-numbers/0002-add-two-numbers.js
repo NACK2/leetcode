4afbb993faf75213e8ef7b0b28be37bd9ef78b20
NACK2 <56712228+NACK2@users.noreply.github.com>
2023-10-23 14:59:35 -0700

    Time: 89 ms (53.54%), Space: 48.1 MB (16.12%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let node = new ListNode();
+    let curr = node;
+    
+    let carry = 0;
+    let tmp = 0;
+    
+    while (l1 || l2 || carry) {
+        tmp = carry;
+        if (l1) {
+            tmp += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            tmp += l2.val;
+            l2 = l2.next;
+        }
+        
+        carry = Math.floor(tmp / 10); // tens digit
+        tmp = tmp % 10; // ones digit
+        
+        curr.next = new ListNode(tmp);
+        curr = curr.next;
+    }
+    
+    return node.next;
+};
